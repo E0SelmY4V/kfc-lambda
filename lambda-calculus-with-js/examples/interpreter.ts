4cfcbae5861713c399c44944d0fd6c6af9de8d9e
E0SelmY4V <yabo1887415157@qq.com>
2026-03-14 15:17:27 +0800

    add: 不使用 Y 组合子的解释器

--- lambda-calculus-with-js/examples/interpreter.ts
+++ lambda-calculus-with-js/examples/interpreter.ts
@@ -1,12 +1,21 @@
-import { Lambda, yC } from '..';
-import { indexed, l0, pushedTail } from './list';
+import { Lambda } from '..';
+import { deletedHead, head, indexed, l0, pushedHead } from './list';
+import { former, latter, tuple } from './tuple';
 
-/**解释器 */
-export const interpreter = yC(s => r => n => s(n(r)))(x => x(l0));
-/**构建一个调用 */
-export const macroCall: Lambda = P => x => y => P(l => x(l)(y(l)));
-/**构建一个函数定义 */
-export const macroFunc: Lambda = P => d => P(l => x => d(pushedTail(l)(x)));
-/**得到一个第 `n` 层的函数的参数 */
-export const macroArg: Lambda = n => P => P(l => indexed(l)(n));
+/**一步步构建的函数 */
+const f: Lambda = t => tuple(head(t(latter))(t(former)))(deletedHead(t(latter)));
+/**构建的初始状态 */
+const r0: Lambda = l => tuple(x => x(l0))(l);
+/**
+ * 解释函数
+ * @param l 数组形式的程序
+ * @link https://www.cnblogs.com/QiFande/p/18984935
+ */
+export const interpreted: Lambda = l => l(latter)(f)(r0(l))(former);
+/**调用指令 */
+export const macroCall: Lambda = p => F => X => p(A => F(A)(X(A)));
+/**函数指令 */
+export const macroFunc: Lambda = p => E => p(A => a => E(pushedHead(A)(a)));
+/**得到外面第 `n` 层函数的参数的指令 */
+export const macroArg: Lambda = n => p => p(A => indexed(A)(n));
 
